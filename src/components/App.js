34b1d5f7b34a079addcc2af6f76bf84593b7a0fc
unnath123 <unnathpgowda@gmail.com>
2023-11-30 01:21:28 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
+import { Routes,Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 const App = () => {
+  const navigate = useNavigate()
   return (
     <div>
-        {/* Do not remove the main div */}
+      <navbar>
+        <ul>
+          <li onClick={()=>navigate("/")}>Home</li>
+          <li onClick={()=>navigate("/About")}>About</li>
+        </ul>
+      </navbar>
+      <Routes>
+        <Route path="/" element={<Home/>} ></Route>
+        <Route path="/About" element={<About/>}></Route>
+      </Routes>
     </div>
   )
 }
